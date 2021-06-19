c0d014c357ba2e8e61d01125666d9432bc880e1f
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2021-06-19 18:37:15 +0700

    [Add] DB connect to baser

--- launcher.py
+++ launcher.py
@@ -1,5 +1,51 @@
 from flask import Flask,render_template
+# from flask_sqlalchemy import SQLALchemy
+from mysql.connector import connect, Error
+
 app = Flask(__name__)
+# db = SQLALchemy(app)
+
+class DBConnect:
+    def __init__(self):
+        self.table = "User_Info"
+        self.conn = connect(
+            host=HOST,
+            username=USER,
+            password=PASS,
+            database=DB
+        )
+        self.cursor = self.conn.cursor()
+
+    def retrieve_all(self):
+        sql_statement = 'SELECT * FROM {}'.format(self.table)
+        userdatas = []
+        try:
+            self.cursor.execute(sql_statement)
+            records = self.cursor.fetchall()
+            for record in records:
+                userdata = {
+                    "username": record[0],
+                    "coin": record[1],
+                    "watchtime": record[2],
+                    "submonth": record[3]
+                }
+                userdatas += [userdata]
+            print(userdatas)
+        except Error as e:
+            print(e)
+
+
+db = DBConnect()
+db.retrieve_all()
+
+
+
+
+# class Item(db.Model):
+#     id = db.Column(db.Intreger(), primary_key=True)
+#     coin= db.Column(db.String(length=30), nullable=False, unique=True)
+#     submonth= db.Column(db.String(length=30), nullable=False,)
+
 
 @app.route('/')
 @app.route('/home')
